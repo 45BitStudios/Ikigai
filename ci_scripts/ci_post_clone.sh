7e3464621244c01e3fb35945d9fb158236261830
Vince Davis <vincent.l.davis@united.com>
2025-07-08 22:21:48 -0500

    script update

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -11,38 +11,10 @@ cd ..
 # ALL STEPS AFTER CLONE PROJECT
 # Generate the Xcode project using XcodeGen
 echo "Generating Xcode project..."
-xcodegen
+xcodegen generate
 echo "Check file on .xcodeproj"
 ls *.xcodeproj
-echo "Check file on project.xcworkspace"
-echo "Check file on xcshareddata"
-ls *.xcodeproj/project.xcworkspace/xcshareddata
-# BASED ON MY EXPERIENCE xcshareddata DIRECTORY IS NOT EXIST, YOU NEED TO CREATE THE DIRECTORY
-mkdir *.xcodeproj/project.xcworkspace/xcshareddata
-# BASED ON MY EXPERIENCE swiftpm DIRECTORY IS NOT EXIST, YOU NEED TO CREATE THE DIRECTORY
-mkdir *.xcodeproj/project.xcworkspace/xcshareddata/swiftpm
-# BASED ON MY EXPERIENCE Package.resolved DIRECTORY IS NOT EXIST, YOU NEED TO CREATE THE DIRECTORY
-touch *.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
-echo "Creating Package.resolved..."
-cat <<EOL > *.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
-# CREATE YOUR EXAMPLE DEPENDENCY HERE, ONLY FOR CREATING Package.resolved
-{
-  "originHash" : "",
-  "pins" : [
-    {
-      "identity" : "",
-      "kind" : "",
-      "location" : "",
-      "state" : {
-        "revision" : "",
-        "version" : ""
-      }
-    }
-  ],
-  "version" : 3
-}
-EOL
-# Resolve package dependencies to generate Package.resolved
+
 echo "Resolving package dependencies..."
 xcodebuild -resolvePackageDependencies -project Ikigai.xcodeproj -scheme IkigaiApp
 # Check if Package.resolved was created
